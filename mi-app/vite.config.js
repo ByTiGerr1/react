610a68fc55bd51a0a51949d1b6cd302aef6700fb
Y0yin <alonso.sanchez@usm.cl>
2025-11-21 20:12:45 -0300

    si 3

--- mi-app/vite.config.js
+++ mi-app/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sassPlugin from "vite-plugin-sass";
+import vitePluginSaas from "vite-plugin-sass";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sassPlugin()],
+  plugins: [react(), vitePluginSaas()],
 });
